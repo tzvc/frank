cd33da736e19771d5fcae798bb4da49a9b9ad390
Théo Champion <contact.theochampion@gmail.com>
2017-08-29 17:07:35 +0000

    initial commit

---
+++ frank.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+# added for frank
+import hardware_mgmt as hw
+import threading
+import queue
+import sys
+import signal
+
+import argparse
+import os.path
+import json
+
+import google.oauth2.credentials
+
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
+from google.assistant.library.file_helpers import existing_file
+
+class SignalHandler:
+    """
+    This object handle signals and stop the workers cleanly
+    """
+    shutdown_flag = None
+    workers = None
+
+    def __init__(self, shutdown_flag, workers):
+        self.shutdown_flag = shutdown_flag
+        self.workers = workers
+
+    def __call__(self, signum, frame):
+        self.shutdown_flag.set()
+        for worker in self.workers:
+            worker.join()
+        sys.exit(0)
+
+        
+def main():
+    num_workers = 1
+    shutdown_flag = threading.Event()
+    event_queue = queue.Queue()
+    workers = [hw.LedMgmtThread(event_queue, shutdown_flag) for i in range(num_workers)]
+
+    signal.signal(signal.SIGINT, SignalHandler(shutdown_flag, workers))
+
+    for worker in workers:
+        worker.start()
+    
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('~/.config'),
+                            'google-oauthlib-tool',
+                            'credentials.json'
+                        ),
+                        help='Path to store and read OAuth2 credentials')
+    args = parser.parse_args()
+    with open(args.credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
+
+    with Assistant(credentials) as assistant:
+        for event in assistant.start():
+            event_queue.put(event)
+
+if __name__ == '__main__':
+    main()
---
+++ hardware_mgmt.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import threading
+import queue
+import math
+
+from google.assistant.library.event import EventType
+
+class HwComponent:
+    def __init__(self, pin, input_mode, is_pwm=False):
+        self.pin = pin
+        self.state = None
+        self.pwm = None
+        self.dc = 0        
+        
+        GPIO.setup(pin, input_mode)
+        if is_pwm:
+            self.pwm = GPIO.PWM(self.pin, 100);
+
+class LedMgmtThread(threading.Thread):
+    """
+    This thread manage integrated LEDs
+    it poll event from a event queue and trigger
+    corresponding animations
+    """
+    def __init__(self, event_queue, shutdown_flag):
+        super().__init__()
+        self.event_queue = event_queue
+        self.shutdown_flag = shutdown_flag
+        self.breathing_speed = 0.0079  # speed of the breathing effect (12/min)
+        self.listening = False
+        self.dc = 0                    # PWM duty cycle
+        self.ct = 0
+        self.sampling_freq = 0.01  # sampling frequency for the breathing function
+        
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        
+        self.leds = {"red" : HwComponent(18, GPIO.OUT, is_pwm=True),
+                     "green" : HwComponent(13, GPIO.OUT, is_pwm=True),
+                     "blue" : HwComponent(12, GPIO.OUT, is_pwm=True)}
+        
+        for led in self.leds.values():
+            led.pwm.start(led.dc)
+        
+    def run(self):
+        while not self.shutdown_flag.is_set():
+            try:
+                event = self.event_queue.get_nowait()
+            except queue.Empty:
+                if not self.listening:                    
+                    if (self.ct > (2*math.pi)):
+                        self.ct = 0
+                    self.dc = (math.exp(math.sin(self.ct)) - 0.36787944)*42.545906412
+                    print(self.dc / 100)
+                    for led in self.leds.values():
+                        led.dc = int(self.dc)
+                        led.pwm.ChangeDutyCycle(self.dc)
+                    self.ct += self.sampling_freq
+                    time.sleep(self.breathing_speed)
+            else:
+                print(event)
+                if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+                    self.fade_in(0.0003, "red")
+                    self.listening = True
+                elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and
+                    event.args and not event.args['with_follow_on_turn']):
+                    self.fade_out(0.005, "red")
+                    self.listening = False
+                self.event_queue.task_done()
+                
+    def fade_in(self, speed, color):
+        done = False
+        while not done:
+            done = True
+            for c, led in self.leds.items():
+                if c != color and led.dc > 0:
+                    led.dc -= 1
+                    led.pwm.ChangeDutyCycle(led.dc)
+                    done = False
+                elif c == color and led.dc < 100:
+                    led.dc += 1
+                    led.pwm.ChangeDutyCycle(led.dc)
+                    done = False
+                print(c + " " + str(led.dc))
+            time.sleep(speed)
+
+    def fade_out(self, speed, color):
+        while self.leds[color].dc > 0:
+            self.leds[color].dc -= 1
+            self.leds[color].pwm.ChangeDutyCycle(self.leds[color].dc)
+            time.sleep(speed)
+        self.ct = (3*math.pi)/2  # minima in range [0;2pi]
+
